2b00791d2154a823ab49ae850629ef879bbbba53
omrege1 <93259671+omrege1@users.noreply.github.com>
2021-10-27 22:15:55 +0530

    Add files via upload

---
+++ bank.sql
@@ -0,0 +1,10 @@
+create database o;
+use o;
+CREATE TABLE BANK ( 
+BANK VARCHAR (20) NOT NULL,  
+ACCNO INT                      
+);  
+
+select * from BANK;
+INSERT into BANK values("HDFC", 1010);
+select * from BANK;
